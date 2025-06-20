6000f207dfdf03dc1592b57287b83d08390d4b83
Bruno Andreotti <brunoandreotti@outlook.com>
2025-06-20 16:40:17 -0300

    Preparação da Deleção de Recursos entre Microservices

--- src/main/java/com/brunoandreotti/course/repositories/CourseUserRepository.java
+++ src/main/java/com/brunoandreotti/course/repositories/CourseUserRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface CourseUserRepository extends JpaRepository<CourseUserModel, UUID> {
 
     Boolean existsByCourseAndUserId(CourseModel course, UUID userId);
+
+    @Query(value = "select * from tb_courses_users where course_id = :courseId", nativeQuery = true)
+    List<CourseUserModel> findAllCourseUserIntoCourse(@Param("courseId")UUID courseId);
 }
--- src/main/java/com/brunoandreotti/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/brunoandreotti/course/services/impl/CourseServiceImpl.java
@@ -7,9 +7,11 @@ import com.brunoandreotti.course.enums.UserType;
 import com.brunoandreotti.course.exceptions.NotFoundException;
 import com.brunoandreotti.course.exceptions.UserStatusException;
 import com.brunoandreotti.course.models.CourseModel;
+import com.brunoandreotti.course.models.CourseUserModel;
 import com.brunoandreotti.course.models.LessonModel;
 import com.brunoandreotti.course.models.ModuleModel;
 import com.brunoandreotti.course.repositories.CourseRepository;
+import com.brunoandreotti.course.repositories.CourseUserRepository;
 import com.brunoandreotti.course.repositories.LessonRepository;
 import com.brunoandreotti.course.repositories.ModuleRepository;
 import com.brunoandreotti.course.services.CourseService;
@@ -32,19 +34,22 @@ public class CourseServiceImpl implements CourseService {
     private final CourseRepository courseRepository;
     private final ModuleRepository moduleRepository;
     private final LessonRepository lessonRepository;
+    private final CourseUserRepository courseUserRepository;
     private final UserClient userClient;
 
-    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository, UserClient userClient) {
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository, CourseUserRepository courseUserRepository, UserClient userClient) {
         this.courseRepository = courseRepository;
         this.moduleRepository = moduleRepository;
         this.lessonRepository = lessonRepository;
+        this.courseUserRepository = courseUserRepository;
         this.userClient = userClient;
     }
 
     @Transactional
     @Override
     public void delete(CourseModel courseModel) {
-        List<ModuleModel> moduleList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        UUID courseId = courseModel.getCourseId();
+        List<ModuleModel> moduleList = moduleRepository.findAllModulesIntoCourse(courseId);
 
         if (!moduleList.isEmpty()) {
 
@@ -59,6 +64,12 @@ public class CourseServiceImpl implements CourseService {
             moduleRepository.deleteAll(moduleList);
             moduleRepository.flush();
         }
+        List<CourseUserModel> courseUserList = courseUserRepository.findAllCourseUserIntoCourse(courseId);
+
+        if (!courseUserList.isEmpty()) {
+            courseUserRepository.deleteAll(courseUserList);
+        }
+
         courseRepository.delete(courseModel);
     }
 
